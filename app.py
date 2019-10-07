aae578156fd500502eb3a682bbd8c0517ff52abe
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-10-06 20:16:19 -0700

    Add Inventory U & D

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/3dgenerates')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/papernoms')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 inventory = db.inventory
@@ -18,16 +18,17 @@ def index():
 @app.route('/inventory/add')
 def item_add():
     '''Add new item to inventory'''
-    return render_template('item_add.html', inventory={})
+    title = "Add Item"
+    return render_template('item_add.html', title=title, inventory={})
 
 @app.route('/inventory', methods=['POST'])
 def inventory_submit():
     '''Submit new item to inventory'''
     item = {
         'name': request.form.get('name'),
-        'description': request.form.get('description'),
+        "price": request.form.get('price'),
         'category': request.form.get('category'),
-        'color': request.form.get('color')
+        'ig_id': request.form.get('ig_id')
     }
 
     item_id = inventory.insert_one(item).inserted_id
@@ -43,17 +44,18 @@ def item_show(item_id):
 @app.route('/inventory/<item_id>/edit')
 def item_edit(item_id):
     '''Show edit form for an item'''
+    title = "Edit Item"
     item = inventory.find_one({'_id': ObjectId(item_id)})
-    return render_template('item_edit.html', item=item)
+    return render_template('item_edit.html', title=title, item=item)
 
 @app.route('/inventory/<item_id>', methods=['POST'])
 def item_update(item_id):
     '''Submit an edited item'''
     updated_item = {
-        'item_name': request.form.get('item_name'),
-        'description': request.form.get('description'),
+        'name': request.form.get('name'),
+        "price": request.form.get('price'),
         'category': request.form.get('category'),
-        'color': request.form.get('color')
+        'ig_id': request.form.get('ig_id')
     }
 
     inventory.update_one(
