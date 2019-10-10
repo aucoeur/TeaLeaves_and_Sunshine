a5d81214245acc92c3f127ee0f974ad94c94a10d
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-10-10 00:33:47 -0700

    Add two tests

--- test.py
+++ test.py
@@ -7,12 +7,14 @@ sample_item = {
     'name': 'Grape',
     'price': '5.21',
     'category': 'fresh',
+    'quantity': None
 }
 
 sample_form_data = {
     'name': sample_item['name'],
     'price': sample_item['price'],
-    'category': sample_item['category']
+    'category': sample_item['category'],
+    'quantity': sample_item['quantity'],
 }
 
 class InventoryTests(TestCase):
@@ -29,6 +31,24 @@ class InventoryTests(TestCase):
         """Test the inventory homepage."""
         result = self.client.get('/')
         self.assertEqual(result.status, '200 OK')
+    
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_show_item(self, mock_find):
+        """Test showing a single item."""
+        mock_find.return_value = sample_item
+
+        result = self.client.get(f'/inventory/{sample_item_id}')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Grape', result.data)
+    
+    @mock.patch('pymongo.collection.Collection.insert_one')
+    def test_submit_item(self, mock_insert):
+        """Test submitting a new item."""
+        result = self.client.post('/inventory', data=sample_form_data)
+
+        # After submitting, should redirect to that playlist's page
+        self.assertEqual(result.status, '302 FOUND')
+        mock_insert.assert_called_with(sample_item)
 
 if __name__ == '__main__':
     unittest_main()
